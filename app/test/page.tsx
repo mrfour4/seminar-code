9ee97e309edd6dde1965ac6311613428de4df5c2
mrfour4 <21521609@gm.uit.edu.vn>
2025-03-22 04:23:25 +0700

    test: check build fail

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,3 +1,10 @@
+// import { Button } from "@/components/ui/button";
+
 export default async function TestPage() {
-    return <div>Test Page</div>;
+    return (
+        <div>
+            Test Page
+            <Button>No import Button</Button>
+        </div>
+    );
 }
