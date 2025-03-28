39a82198022ee8acba6c26cd4e33ce023bcdbaaf
mrfour4 <21521609@gm.uit.edu.vn>
2025-03-29 02:18:41 +0700

    feat: test github actions

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,7 +4,7 @@ export default async function TestPage() {
     return (
         <div>
             Test Page
-            <Button>Click</Button>
+            <Button>Click me</Button>
         </div>
     );
 }
