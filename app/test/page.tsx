05ad60278f408eb61a83a797887b222a21561c8e
Le Qui Tu <95972670+mrfour4@users.noreply.github.com>
2025-03-29 10:23:44 +0700

    Save changes before checkout (#19)

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default async function TestPage() {
     return (
         <div>
-            Change Page
+            Test Page ...
             <Button>Click me!</Button>
         </div>
     );
