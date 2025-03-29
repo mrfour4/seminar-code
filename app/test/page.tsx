fcdf36d8f12df737f0f373f4cc93ce84045853d0
Le Qui Tu <95972670+mrfour4@users.noreply.github.com>
2025-03-29 16:08:31 +0700

    Test healthy state (#26)

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default async function TestPage() {
     return (
         <div>
-             Test Healthy 
+             Test Healthy State
             <Button>Click me!</Button>
         </div>
     );
