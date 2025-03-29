1f7e407090e7b0bf2aeb0f0063c088c2a8948f70
mrfour4 <21521609@gm.uit.edu.vn>
2025-03-29 20:54:59 +0700

    test: gitops workflow

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default async function TestPage() {
     return (
         <div>
-             Test Healthy State
+             Test 
             <Button>Click me!</Button>
         </div>
     );
