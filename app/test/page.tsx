fc7a1c58609772fcc32649c876e24d714f7e1f61
Le Qui Tu <95972670+mrfour4@users.noreply.github.com>
2025-03-29 13:41:14 +0700

    Test degraded state (#23)

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default async function TestPage() {
     return (
         <div>
-             Test degraded state
+             Test degraded state 2
             <Button>Click me!</Button>
         </div>
     );
