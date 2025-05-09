1118ef7486d0d9542453a6a3d364a6ddc2606fe4
Le Qui Tu <95972670+mrfour4@users.noreply.github.com>
2025-05-09 07:49:17 +0700

    test: change title for test page (#35)

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function TestPage() {
     return (
-        <div>
-             Test 123
-            <Button>Click me!</Button>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold">Test Page</h1>
+            <p className="my-4">This is a test page.</p>
+            <Button asChild>
+                <Link href="/">Go to Home</Link>
+            </Button>
         </div>
     );
 }
