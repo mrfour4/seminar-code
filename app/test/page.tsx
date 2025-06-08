4b314ffe897c10c73b8ab002a7d4411e58fcb6dc
Le Qui Tu <95972670+mrfour4@users.noreply.github.com>
2025-06-08 12:44:58 +0700

    Test/git notify (#39)
    * test: change test page title
    
    * test: change test page title

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default async function TestPage() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold">Test Page Notifycation</h1>
+            <h1 className="text-2xl font-bold">Notify</h1>
             <p className="my-4">This is a test page.</p>
             <Button asChild>
                 <Link href="/">Go to Home</Link>
